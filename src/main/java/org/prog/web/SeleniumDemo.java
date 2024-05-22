fe4eaddf6f925802679aa43db7aa513b3ba8e9a1
dianakovalenko <ruhesee@gmai.com>
2024-05-22 21:09:33 +0200

    go

--- src/main/java/org/prog/web/SeleniumDemo.java
+++ src/main/java/org/prog/web/SeleniumDemo.java
@@ -4,45 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class SeleniumDemo {
 
     /*
-     * 1. Open browser +
-     * 2. Load allo.ua +
-     * 3. Click Акції +
-     * 4. Click Present +
-     * 5. Click first present +
-     * 6. Print present's name
+     * 1. Open browser
+     * 2. Load allo.ua
+     * 3. Press profile btn
+     * 4. Select login/password
+     * 5. Set incorrect login/password
+     * 6. Press login
+     * 7. Print error messages
      */
 
     public static void main(String[] args) {
-        //start browser
+        //start browse
         WebDriver driver = new ChromeDriver();
         try {
             //full screen
             driver.manage().window().maximize();
             //load allo ua
             driver.get("https://allo.ua/");
-            //select discounts
-            WebElement discountsBtn = driver.findElement(By.partialLinkText("Акції"));
-            discountsBtn.click();
-            // click presents
-            WebElement presentBtn = driver.findElement(By.partialLinkText("Подарунки"));
-            presentBtn.click();
-            //find promo items parent element
-            WebElement promoParent = driver.findElement(By.className("promo-list__items"));
-            //find and click 1st promo item
-            List<WebElement> promoList = promoParent.findElements(By.tagName("a"));
-            WebElement firstPromo = promoList.get(0);
-            firstPromo.click();
-            // print promo title
-            WebElement promoTitle = driver.findElement(By.className("promo-info__title"));
-            System.out.println(promoTitle.getText());
+            // Press profile button
+            WebElement profileBtn = driver.findElement(By.className("mh-profile"));
+            profileBtn.click();
+            WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(3L));
+            WebElement pwdBtnWait = driverWait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.className("auth__enter-password")));
+            pwdBtnWait.click();
+            //Press login
+            WebElement loginBtn = driverWait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.className("a-button--primary")));
+            loginBtn.click();
+            //Print error messages
+            System.out.println("wrong password!");
         } finally {
             driver.quit();
         }
     }
 }
+
